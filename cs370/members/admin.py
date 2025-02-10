aa3c106a82e6751da2823a7679bd2f5d83a099c5
Olin Gilster <olin.gilster@icloud.com>
2025-02-10 12:15:57 -0500

    create basic admin file

--- cs370/members/admin.py
+++ cs370/members/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+# import the database models that have been used
+from .models import Member, User, Transaction 
 
-# Register your models here.
+class MemberAdmin(admin.ModelAdmin):
+    pass
+
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+class TransactionAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Member, MemberAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(Transaction, TransactionAdmin)
