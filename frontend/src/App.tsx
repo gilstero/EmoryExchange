8915790e5ba03a68c43e8c79c60216689b1debd9
Noah Goodman <>
2025-02-17 13:59:53 -0500

    Integrated navbar to the marketplace page. Aligned to edge of the screen. Added placeholder account button.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,34 @@
-import React, { useState } from 'react'
-import Navbar from './components/Navbar'
-import Main from './components/Main'
-import Description from './components/Description'
-import Footer from './components/Footer'
-import { Routes, Route } from 'react-router'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import Description from "./components/Description";
+import Footer from "./components/Footer";
+import { Routes, Route } from "react-router";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Marketplace from "./pages/Marketplace";
 
 function App() {
-
   return (
     <>
       <Routes>
-        <Route path="/" element={
-            <>       
+        <Route
+          path="/"
+          element={
+            <>
               <Navbar />
               <Main />
               <Description />
               <Footer />
             </>
-          } />
+          }
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/marketplace" element={<Marketplace />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
